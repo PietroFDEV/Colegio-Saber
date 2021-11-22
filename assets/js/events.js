119f05514b48d46903da26388ed53e0d05e78459
Pitocoloco <72633176+Pitocoloco@users.noreply.github.com>
2021-11-22 15:59:22 -0300

    final colegio

---
+++ assets/js/events.js
@@ -0,0 +1,66 @@
+class MobileNavBar {
+    constructor(mobileMenu, navList, navLinks){
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    animateLinks() {
+        this.navLinks.forEach((link, index) => {
+        link.style.animation
+            ? (link.style.animation = "")
+            : (link.style.animation = 'navLinkFade 0.9s ease forwards 0.1s');
+        });
+    }
+
+    handleClick(){
+        this.navList.classList.toggle(this.activeClass);
+        this.animateLinks();
+    }
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+    init() {
+        if (this.mobileMenu){
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
+
+const mobileNavBar = new MobileNavBar(
+    '.mobile-menu',
+    '.nav-list',
+    '.nav-list li',
+);
+mobileNavBar.init();
+
+
+/* menu */
+
+
+
+var buttonB = document.getElementById("action-btnB");
+
+buttonB.addEventListener("click", function(){
+    
+    var containerB = document.getElementById("containerB");
+
+    containerB.classList.toggle("hide");
+})
+
+var button = document.getElementById("action-btn");
+
+button.addEventListener("click", function(){
+    
+    var container = document.getElementById("container");
+
+    container.classList.toggle("hide");
+})
+
+/* SLIDE */
+
